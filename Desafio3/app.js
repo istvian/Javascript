aab9a6c493b174c5206a30635c63d4ff28ca5bd5
istvian <e.r94@outlook.cl>
2021-09-22 11:57:31 -0300

    Agrego desafio 3

---
+++ Desafio3/app.js
@@ -0,0 +1,17 @@
+ alert("Desafio N°3 - CREAR UN ALGORITMO UTILIZANDO UN CICLO \nSecuencia de Fibonacci");
+ var previo = 0;
+ var actual = 1;
+ var resultado;
+ let output = document.getElementById("output");
+ try {
+     var iteraciones = Number(prompt("¿Cuantas iteraciones deseas? (Recomiendo que no sean muchas xD)"));
+     for (let i = 0; i < iteraciones; i++) {
+         resultado = previo + actual;
+         console.log(`${previo}+${actual}=${resultado}`);
+         output.innerText += `${previo}+${actual}=${parseInt(resultado)}\n`;
+         previo = actual;
+         actual = resultado;
+     }
+ } catch {
+     alert("Ocurrio un error");
+ }
